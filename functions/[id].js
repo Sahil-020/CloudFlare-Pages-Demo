a5b7e7168815edab1ccf97b6b2d7a346bdc48d40
Sahil-020 <80909385+Sahil-020@users.noreply.github.com>
2021-12-03 20:34:03 +0530

    Update [id].js

--- functions/[id].js
+++ functions/[id].js
@@ -1,5 +1,5 @@
 export const onRequestGet = (context) => {
   let { params } = context
-// return new Response(`The id : ${JSON.stringify(param)}`)
-  return new Response(`The id : ${params}`,{ headers:{ "content-type":"application/json" } })
+return new Response(`The id : ${params}`)
+//   return new Response(`The id : ${params}`,{ headers:{ "content-type":"application/json" } })
 }
