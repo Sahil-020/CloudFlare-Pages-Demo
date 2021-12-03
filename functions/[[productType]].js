ce5be0dfa1d5c940dc30f14afd10ce2cf54eb1b3
Sahil-020 <80909385+Sahil-020@users.noreply.github.com>
2021-12-03 21:23:16 +0530

    Update [[productType]].js

--- functions/[[productType]].js
+++ functions/[[productType]].js
@@ -1,6 +1,9 @@
 
 export const onRequestGet = (context) => {
   let { params } = context
+ var myHeaders = new Headers({
+    'Content-Type': 'application/json'
+});
 // return new Response(`The id : ${JSON.stringify(param)}`)
-  return new Response(`The id : ${ params }`,{ headers:{ "content-type":"application/json" } })
+  return new Response(`The id : ${ params }`,{ headers:myHeaders })
 }
