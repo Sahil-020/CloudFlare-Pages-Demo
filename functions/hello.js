8eddee796470e1c79e926cf204e230053b28c262
Sahil-020 <80909385+Sahil-020@users.noreply.github.com>
2021-12-03 15:06:14 +0530

    Update hello.js

--- functions/hello.js
+++ functions/hello.js
@@ -1,10 +1,10 @@
-export const onRequestGet = ({request}) => {
+export const onRequestGet = async ({request}) => {
   let {url} = await request.json()
   return new Response(`Request Url is : ${url}`)
 }
 
 // POST requests to /filename with a JSON-encoded body would return "Hello, <name>!"
-export const onRequestPost = async ({ request }) => {
-  const { name } = await request.json()
-  return new Response(`Hello, ${name}!`)
-}
+// export const onRequestPost = async ({ request }) => {
+//   const { name } = await request.json()
+//   return new Response(`Hello, ${name}!`)
+// }
