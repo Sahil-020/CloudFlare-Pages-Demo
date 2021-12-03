5bb62eb69740810fefc5d9f9f9a9c7e75d5de39b
Sahil-020 <80909385+Sahil-020@users.noreply.github.com>
2021-12-03 20:26:29 +0530

    Update [[productType]].js

--- functions/[[productType]].js
+++ functions/[[productType]].js
@@ -1,1 +1,6 @@
 
+export const onRequestGet = (context) => {
+  let { params } = context
+// return new Response(`The id : ${JSON.stringify(param)}`)
+  return new Response(`The id : ${params}`,{ headers:{ "content-type":"application/json" } })
+}
