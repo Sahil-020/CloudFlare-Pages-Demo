90cfcf8dedc571e470269d75cb1ec2935f35825c
Sahil-020 <80909385+Sahil-020@users.noreply.github.com>
2021-12-06 15:30:28 +0530

    Update [id].js

--- functions/[id].js
+++ functions/[id].js
@@ -1,3 +1,19 @@
+async function gatherResponse(response) {
+  const { headers } = response
+  const contentType = headers.get("content-type") || ""
+  if (contentType.includes("application/json")) {
+    return JSON.stringify(await response.json())
+  }
+  else if (contentType.includes("application/text")) {
+    return response.text()
+  }
+  else if (contentType.includes("text/html")) {
+    return response.text()
+  }
+  else {
+    return response.text()
+  }
+}
 export const onRequestGet = (context) => {
   let { params } = context
 return new Response(`The id : ${JSON.stringify(params)}`)
