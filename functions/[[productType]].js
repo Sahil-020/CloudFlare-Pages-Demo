be9c323047b6918a4d135d3064a10cdcef280732
Sahil-020 <80909385+Sahil-020@users.noreply.github.com>
2021-12-06 19:59:21 +0530

    Update [[productType]].js

--- functions/[[productType]].js
+++ functions/[[productType]].js
@@ -26,22 +26,21 @@ export const onRequestGet = async (context) => {
     'Content-Type': 'application/json'
  });
  let response
-//  if(producType[1] === "J"){
-//    const urlFetch = `https://${AppUrl}/${JewelrySerialApp}/_doc/${productType[0]}/_source`
-//    response = await fetch(urlFetch, init) 
-//  } else if(producType[1] === "D"){
-//    const urlFetch = `https://${AppUrl}/${DiamondSerialApp}/_doc/${productType[0]}/_source`
-//    response = await fetch(urlFetch, init) 
-//  }else if(producType[1] === "G"){
-//    const urlFetch = `https://${AppUrl}/${GemstoneSerialApp}/_doc/${productType[0]}/_source`
-//    response = await fetch(urlFetch, init) 
-//  }
-//  if (response.status === 400 || response.status === 404){
-//    return new Response(`${ JSON.stringify(response.status) } - ${ JSON.stringify(response.statusText) }`)
-//  }
-//  let results = await gatherResponse(response)
-//  return new Response(`result : ${JSON.stringify(results)}`)
- return new Response(`The id : ${ JSON.stringify(productType) }`)
+ if(producType[1] === "J"){
+   const urlFetch = `https://${AppUrl}/${JewelrySerialApp}/_doc/${productType[0]}/_source`
+   response = await fetch(urlFetch, init) 
+ } else if(producType[1] === "D"){
+   const urlFetch = `https://${AppUrl}/${DiamondSerialApp}/_doc/${productType[0]}/_source`
+   response = await fetch(urlFetch, init) 
+ }else if(producType[1] === "G"){
+   const urlFetch = `https://${AppUrl}/${GemstoneSerialApp}/_doc/${productType[0]}/_source`
+   response = await fetch(urlFetch, init) 
+ }
+ if (response.status === 400 || response.status === 404){
+   return new Response(`${ JSON.stringify(response.status) } - ${ JSON.stringify(response.statusText) }`)
+ }
+ let results = await gatherResponse(response)
+ return new Response(`The id : ${ JSON.stringify(productType) }\n\nresult : ${JSON.stringify(results)}`)
 //  return new Response(`The id : ${JSON.stringify(params)}\n\n${JSON.stringify(response)}\n\n${JSON.stringify(results)}`,{
 //       headers: {
 //         "content-type": "application/json;charset=UTF-8"
